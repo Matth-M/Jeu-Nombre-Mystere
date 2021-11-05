02adfdc55e8f51758bfc91ed5338ce1fb3dd29a6
Matth-M <matthieu.matsima@gmail.com>
2021-11-05 15:49:04 +0100

    Ajout du main

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "tableaux.h"
+#include <string.h>
+
+int main()
+{
+    char chaine1[100] = "Salut ", chaine2[] = "Mateo21";
+    printf("chaine1 vaut : %s\n", strcat(chaine1, chaine2));
+    printf("chaine2 vaut toujours : %s\n", chaine2);
+    return 0;
+}
+
